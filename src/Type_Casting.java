e23141843b6256d53da8d69d14d5125f04db66eb
SanjeebRay2003 <sanjeebray2003@gmail.com>
2024-12-27 21:04:14 +0530

    DSA with Java

---
+++ src/Type_Casting.java
@@ -0,0 +1,12 @@
+
+public class Type_Casting {
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
