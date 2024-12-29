a243878ecdca953ab192745bbca9bcab73746817
SanjeebRay2003 <sanjeebray2003@gmail.com>
2024-12-29 21:47:15 +0530

    Methods

---
+++ src/Methods/method.java
@@ -0,0 +1,19 @@
+package Methods;
+
+public class method {
+
+    static  void greeting(String hello,String bye){
+        System.out.println(hello);
+        System.out.println(bye);
+    }
+
+    static void India(){
+        System.out.println("Every citizen of India are Indians");
+    }
+
+    public static void main(String[] args) {
+
+        greeting(  "Hello, How are you ?","Bye, See you later");
+        India();
+    }
+}
