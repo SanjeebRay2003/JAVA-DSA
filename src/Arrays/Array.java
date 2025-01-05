7d77c82fccf09bb2ac64b1242ec45f783944ab1d
SanjeebRay2003 <sanjeebray2003@gmail.com>
2025-01-05 19:19:35 +0530

    Arrays

---
+++ src/Arrays/Array.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+        System.out.println(numbers[3]);
+
+        for (int e:numbers){
+            System.out.print(e+" ");
+        }
+
+
+    }
+}
