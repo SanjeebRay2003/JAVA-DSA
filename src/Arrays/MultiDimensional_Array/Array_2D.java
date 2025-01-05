523b6d888e52d7bd43949dbd04bdd6fdf8a1b878
SanjeebRay2003 <sanjeebray2003@gmail.com>
2025-01-05 22:13:43 +0530

    2D Array

--- src/Arrays/MultiDimensional_Array/Array_2D.java
+++ src/Arrays/MultiDimensional_Array/Array_2D.java
@@ -7,24 +7,29 @@ public class Array_2D {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
         int[][] array = new int[3][3];
+        System.out.println("Enter elements of 2D array : ");
+
         for (int row = 0; row < array.length; row++) {
             for (int column = 0; column < array[row].length; column++) {
                 array[row][column] = input.nextInt();
             }
         }
+
+
         System.out.println("Output : ");
 
-        for (int row = 0; row < array.length; row++) {
-            for (int column = 0; column < array[row].length; column++) {
-                System.out.print(array[row][column] + " ");
-            }
-            System.out.println();
-        }
-
-        for (int row = 0; row < array.length; row++) {
-            System.out.println(Arrays.toString(array[row]));
-        }
+//        for (int row = 0; row < array.length; row++) {
+//            for (int column = 0; column < array[row].length; column++) {
+//                System.out.print(array[row][column] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int row = 0; row < array.length; row++) {
+//            System.out.println(Arrays.toString(array[row]));
+//        }
 
         for (int[] arr : array) {
             System.out.println(Arrays.toString(arr));
---
+++ src/Arrays/MultiDimensional_Array/NO_column.java
@@ -0,0 +1,28 @@
+package Arrays.MultiDimensional_Array;
+
+import java.util.Arrays;
+
+public class NO_column {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1,8,6,78},
+                {8,6,9},
+                {8},
+                {8,920},
+                {78,96,45,885,965,44}
+        };
+
+        System.out.println("All elements of array :");
+
+//        for (int row = 0;row<array.length;row++){
+//            for (int column = 0;column<array[row].length;column++){
+//                System.out.print(array[row][column]+ " ");
+//            }
+//            System.out.println();
+//        }
+
+        for (int[] e: array){
+            System.out.println(Arrays.toString(e));
+        }
+    }
+}
