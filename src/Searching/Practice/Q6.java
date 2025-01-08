bbd6b79b1cc82d4743b575d1656ef9a44a50a480
SanjeebRay2003 <sanjeebray2003@gmail.com>
2025-01-08 21:04:47 +0530

    Searching

--- src/Searching/Practice/Q6.java
+++ src/Searching/Practice/Q6.java
@@ -1,25 +1,43 @@
 package Searching.Practice;
 
+import java.util.Arrays;
+
+// search the index of target element in 2d array and return a 1d array of index
 public class Q6 {
     public static void main(String[] args) {
         int[][] marks = {
-                {45,86,78,54,68,45},
+                {45,86,54,68,45},
                 {78,85,96,84},
                 {96,87,88,99}
         };
 
         int target = 78;
-        System.out.println(search(marks,target));
+        int[] newArr = search(marks,78);
+        System.out.println(Arrays.toString(newArr));
+        System.out.println(maxMarks(marks));
     }
 
-    static int search(int[][] marks,int target){
+    static int[] search(int[][] marks,int target){
         for (int i = 0;i<marks.length;i++){
             for (int j =0;j<marks[i].length;j++){
-                if (j == target){
-                    return j;
+                if (marks[i][j] == target){
+                    return new int[]{i,j} ;
                 }
             }
         }
-        return -1;
+        return new int[]{-1,-1};
+    }
+
+    // Returns maximum value of element
+    static int maxMarks(int[][] marks){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0;i<marks.length;i++){
+            for (int j =0;j<marks[i].length;j++){
+                if (marks[i][j] > max){
+                    max = marks[i][j];
+                }
+            }
+        }
+        return max;
     }
 }
