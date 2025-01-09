5bd7bd8fee5182b279af042fa4292a6053066e7e
SanjeebRay2003 <sanjeebray2003@gmail.com>
2025-01-09 22:27:23 +0530

    Searching

---
+++ src/Searching/Practice/Q9.java
@@ -0,0 +1,31 @@
+package Searching.Practice;
+
+public class Q9 {
+    public static void main(String[] args) {
+
+        int[][] accounts = {
+                {4,2,3},
+                {3,2,1},
+                {4,5,2},
+                {5,0,4}
+        };
+
+        int maxWealth = maximumWealth(accounts);
+        System.out.println(maxWealth);
+    }
+
+    static int maximumWealth(int[][] accounts) {
+
+        int ans = Integer.MIN_VALUE;
+        for ( int i = 0;i<accounts.length;i++){
+            int sum = 0;
+            for (int j = 0;j<accounts[i].length;j++){
+                sum += accounts[i][j];
+            }
+            if (sum>ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
